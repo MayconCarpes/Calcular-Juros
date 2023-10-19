a494dedef606666c11d2301a388aa0b6d53b1a07
MayconCarpes <62576871+MayconCarpes@users.noreply.github.com>
2023-10-18 22:39:15 -0300

    Add files via upload

---
+++ CalculadoraJurosFinanceiro/CalculadoraJurosFinanceiro/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CalculadoraJurosFinanceiro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrincipal = new System.Windows.Forms.TextBox();
+            this.txttaxaDeJurosMensal = new System.Windows.Forms.TextBox();
+            this.txtnumeroDeParcelas = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblJuros = new System.Windows.Forms.Label();
+            this.lblPago = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor Principal do Empréstimo:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Taxa de Juros Anual (%):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número de Parcelas:";
+            // 
+            // txtPrincipal
+            // 
+            this.txtPrincipal.Location = new System.Drawing.Point(189, 58);
+            this.txtPrincipal.Name = "txtPrincipal";
+            this.txtPrincipal.Size = new System.Drawing.Size(140, 20);
+            this.txtPrincipal.TabIndex = 3;
+            // 
+            // txttaxaDeJurosMensal
+            // 
+            this.txttaxaDeJurosMensal.Location = new System.Drawing.Point(189, 127);
+            this.txttaxaDeJurosMensal.Name = "txttaxaDeJurosMensal";
+            this.txttaxaDeJurosMensal.Size = new System.Drawing.Size(140, 20);
+            this.txttaxaDeJurosMensal.TabIndex = 4;
+            // 
+            // txtnumeroDeParcelas
+            // 
+            this.txtnumeroDeParcelas.Location = new System.Drawing.Point(189, 195);
+            this.txtnumeroDeParcelas.Name = "txtnumeroDeParcelas";
+            this.txtnumeroDeParcelas.Size = new System.Drawing.Size(140, 20);
+            this.txtnumeroDeParcelas.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalcular.Image = global::CalculadoraJurosFinanceiro.Properties.Resources.matematicas;
+            this.btnCalcular.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCalcular.Location = new System.Drawing.Point(135, 267);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(99, 45);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("PMingLiU-ExtB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(461, 76);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(165, 19);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "O Cálculo Dos Juros:";
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // lblJuros
+            // 
+            this.lblJuros.AutoSize = true;
+            this.lblJuros.Font = new System.Drawing.Font("PMingLiU-ExtB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJuros.Location = new System.Drawing.Point(461, 175);
+            this.lblJuros.Name = "lblJuros";
+            this.lblJuros.Size = new System.Drawing.Size(93, 19);
+            this.lblJuros.TabIndex = 10;
+            this.lblJuros.Text = "Total Pago:";
+            this.lblJuros.Click += new System.EventHandler(this.lblJuros_Click);
+            // 
+            // lblPago
+            // 
+            this.lblPago.AutoSize = true;
+            this.lblPago.Font = new System.Drawing.Font("PMingLiU-ExtB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPago.Location = new System.Drawing.Point(461, 124);
+            this.lblPago.Name = "lblPago";
+            this.lblPago.Size = new System.Drawing.Size(120, 19);
+            this.lblPago.TabIndex = 11;
+            this.lblPago.Text = "Total em juros:";
+            this.lblPago.Click += new System.EventHandler(this.lblPago_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPago);
+            this.Controls.Add(this.lblJuros);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtnumeroDeParcelas);
+            this.Controls.Add(this.txttaxaDeJurosMensal);
+            this.Controls.Add(this.txtPrincipal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrincipal;
+        private System.Windows.Forms.TextBox txttaxaDeJurosMensal;
+        private System.Windows.Forms.TextBox txtnumeroDeParcelas;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblJuros;
+        private System.Windows.Forms.Label lblPago;
+    }
+}
+
---
+++ CalculadoraJurosFinanceiro/CalculadoraJurosFinanceiro/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraJurosFinanceiro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //P= (L*i) / (1−(1+i)^−n)
+        /*valorDaParcela = principal * (taxaDeJurosMensal 
+         * / (1 - Math.Pow(1 + taxaDeJurosMensal, -numeroDeParcelas)));*/
+        private void label4_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+     /*   
+    Onde:P é o valor da parcela mensal (valorDaParcela).      
+    L é o valor principal do empréstimo (principal).       
+    i é a taxa de juros mensal (taxaDeJurosMensal).
+    Pegue a taxa anual e divida por 12.       
+    n é o número total de parcelas (numeroDeParcelas).*/
+
+        //debug em cima da linha no atalho f9
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double taxaAnual = Convert.ToDouble(txttaxaDeJurosMensal.Text);
+            double principal = Convert.ToDouble(txtPrincipal.Text);
+            double numeroParcela = Convert.ToDouble(txtnumeroDeParcelas.Text);
+
+            double taxaDeJurosMensal = (taxaAnual / 100) / 12;
+            
+            //                                P= (L*i)              /          (1−(1+i)^−n)
+            double valorDaParcela = principal * (taxaDeJurosMensal / (1 - Math.Pow(1 + taxaDeJurosMensal, -numeroParcela) ));
+            double TotalDeJuros = numeroParcela * valorDaParcela;
+            double valorTotal = TotalDeJuros - principal;
+
+            lblResultado.Text = "Valor da Parcela: " + valorDaParcela.ToString("N2");
+            lblJuros.Text = "Total em juros: " + TotalDeJuros.ToString("N2");
+            lblPago.Text = "Total Pago: " + valorTotal.ToString("N2");
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblJuros_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPago_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
